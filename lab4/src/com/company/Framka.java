e599117d2a4f35d54cb20e849256696c39d16d76
Piotr2137 <piotrrachx@gmail.com>
2021-12-07 16:58:21 +0100

    ruszanie się przycisku

--- lab4/src/com/company/Framka.java
+++ lab4/src/com/company/Framka.java
@@ -2,8 +2,12 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Random;
+
 
 public class Framka extends JFrame{
 
@@ -14,25 +18,48 @@ public class Framka extends JFrame{
         setSize(688,500);
         setLocationRelativeTo(null);
         JPanel panel = new JPanel();
-        panel.setBounds(0,0,660,483);
+        panel.setBounds(0,0,630,500);
         setContentPane(panel);
         panel.setLayout(null);
 
-        int x, y;
+        final int[] x = {0};
+        final int[] y = {0};
+        final Random random = new Random();
 
-        JButton btnRunButton = new JButton("Run");
+        final JButton btnRunButton = new JButton("Run");
+
         btnRunButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
                 super.mouseEntered(e);
+
+                if(x[0]<340 || y[0]>170) {
+                    x[0] = random.nextInt(518);
+                    y[0] = random.nextInt(412);
+                    btnRunButton.setLocation(x[0], y[0]);
+                }
             }
         });
 
-        btnRunButton.setBounds(0,412,156,71);
+        btnRunButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnRunButton.setLocation(0,412);
+            }
+        });
+
+        btnRunButton.setBounds(0,412,156,50);
         panel.add(btnRunButton);
 
         JButton btnCancel = new JButton("Cancel");
-        btnCancel.setBounds(504,412,156,71);
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnRunButton.setLocation(0,412);
+            }
+        });
+
+        btnCancel.setBounds(518,412,156,50);
         panel.add(btnCancel);
 
     }
